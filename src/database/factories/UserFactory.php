c1b29f3de55f24eb99f118d07d613bee6d218cae
egg <titanegif@gmail.com>
2025-11-21 13:26:12 +0700

    factory update

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-use function fake;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -25,8 +24,9 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            // FIX: Menggunakan $this->faker untuk mengatasi Call to undefined function fake() di production.
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
